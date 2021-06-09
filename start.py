bdd505d6ac173e99c1bdbf60bed563438e4598cb
Lukas Michael Schmidt l.schmidt@tu-berlin.de <tu-lschmidt@yogurt.mlmip.de>
2021-06-09 12:05:42 +0200

    Config path and loading updated.

--- start.py
+++ start.py
@@ -11,7 +11,7 @@ def get_args():
     """ Get command-line arguments. """
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--config", type=str, default='default.json', help="Name of a json config file within configs folder. (default: 'default.json')")
+    parser.add_argument("--config", type=str, default='default', help="Name of a python config file within configs folder. (default: 'default')")
     parser.add_argument("--train", default=False, action='store_true', help="Train the gan. (default: False)")
 
     return parser.parse_args()
@@ -24,10 +24,9 @@ if __name__ == '__main__':
 
     # Initialize abs path to config folder and data/x
     abs_path = pathlib.Path().absolute()
-    conf_path = os.path.join(abs_path, 'configs/')
 
     # Initialize path to config and load dict
-    config_path = os.path.join(conf_path, args.config)
+    config_path = 'configs.' + args.config
     config = load_config(config_path)
 
     if args.train:
